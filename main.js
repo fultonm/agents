fdad14b742bc75280c186c52348e5e065f483ba6
Michael Fulton <mike@michaelfulton.co>
2019-03-03 12:33:24 -0800

    returns food

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ function Agent(game, home, food) {
     this.home = home
     this.food = food
     this.searchingFood = true
+    this.returnedFood = false
     this.successfulTrail = []
     this.pos = Object.assign({}, this.home.pos)
     this.trail = []
     this.trail.push(Object.assign({}, this.pos))
     this.decisionTimer = 0
-    this.decisionInterval = 2
+    this.decisionInterval = 3
     var vec = getRandomVec()
     this.dx = vec.x
     this.dy = vec.y
-    this.speed = 4
+    this.speed = 2.5
 }
 
 Agent.prototype.constructor = Agent
@@ -20,13 +21,34 @@ Agent.prototype.constructor = Agent
 Agent.prototype.update = function () {
     if (this.searchingFood) {
         this.decisionTimer += this.game.clockTick
-        if (this.decisionTimer > this.decisionInterval) {
+        if (this.decisionTimer > this.decisionInterval / this.speed) {
             this.decisionTimer = 0
             this.trail.push(Object.assign({}, this.pos))
             var randomVec = getRandomVec()
             this.dx = randomVec.x
             this.dy = randomVec.y
         }
+        if (distance(this.pos, this.food.pos) < 50) {
+            this.trail.push(Object.assign({}, this.pos))
+            this.successfulTrail.push(Object.assign({}, this.pos))
+            this.searchingFood = false
+            this.speed = 1
+        }
+    } else if (this.returnedFood == false) {
+        var nextCrumb = this.trail[Math.max(this.trail.length - this.successfulTrail.length * 2, 0)]
+        var vec = vectorToward(nextCrumb, this.pos)
+        this.dx = vec.x
+        this.dy = vec.y
+        if (distance(this.pos, nextCrumb) < 25) {
+            if (this.trail[0] === nextCrumb) {
+                this.returnedFood = true
+            }
+            this.successfulTrail.push(Object.assign({}, this.pos))
+
+        }
+
+    }
+    if (this.returnedFood == false) {
         this.pos.x += this.dx * this.speed
         this.pos.y += this.dy * this.speed
         if (this.pos.x > this.game.ctx.canvas.width || this.pos.x < 0) {
@@ -35,39 +57,20 @@ Agent.prototype.update = function () {
         if (this.pos.y > this.game.ctx.canvas.height || this.pos.y < 0) {
             this.dy *= -1
         }
-        if (distance(this.pos, this.food.pos) < 10) {
-            this.successfulTrail.push(Object.assign({}, this.pos))
-            this.searchingFood = false
-        }
-    } else {
-        var nextCrumb = this.trail[this.trail.length - this.successfulTrail.length * 2]
-        var vec = vectorToward(nextCrumb, this.pos)
-        this.dx = vec.x
-        this.dy = vec.y
-        if (distance(this.pos, nextCrumb) < 10) {
-            this.successfulTrail.push(Object.assign({}, this.pos))
-        }
     }
 }
 
 Agent.prototype.draw = function () {
     var ctx = this.game.ctx
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(this.pos.x, this.pos.y, 8, 8)
+    ctx.fillRect(this.pos.x, this.pos.y, 12, 12)
     ctx.setLineDash([5, 3]);/*dashes are 5px and spaces are 3px*/
-    if (this.searchingFood) {
-        ctx.fillStyle = "#E0E0E0";
-        ctx.strokeStyle = "#E0E0E0";
-    } else {
-        ctx.fillStyle = "#32CD32";
-        ctx.strokeStyle = "#32CD32";
-    }
-
-
+    ctx.fillStyle = "#E0E0E0";
+    ctx.strokeStyle = "#E0E0E0";
     var prevCrumb = this.trail[0]
     for (var i = 1; i < this.trail.length; i++) {
         var thisCrumb = this.trail[i]
-        ctx.fillRect(thisCrumb.x, thisCrumb.y, 3, 3)
+        ctx.fillRect(thisCrumb.x, thisCrumb.y, 4, 4)
 
         ctx.beginPath();
         ctx.moveTo(prevCrumb.x, prevCrumb.y);
@@ -75,17 +78,48 @@ Agent.prototype.draw = function () {
         ctx.stroke();
         prevCrumb = thisCrumb
     }
-    ctx.beginPath();
-    ctx.moveTo(prevCrumb.x, prevCrumb.y);
-    ctx.lineTo(this.pos.x, this.pos.y);
-    ctx.stroke();
+
+    if (this.searchingFood) {
+        ctx.beginPath();
+        ctx.moveTo(prevCrumb.x, prevCrumb.y);
+        ctx.lineTo(this.pos.x, this.pos.y);
+        ctx.stroke();
+    }
+
+    ctx.fillStyle = "#32CD32";
+    ctx.strokeStyle = "#32CD32";
+
+    for (var i = 1; i < this.successfulTrail.length; i++) {
+        var thisCrumb = this.successfulTrail[i]
+        ctx.fillRect(thisCrumb.x, thisCrumb.y, 4, 4)
+
+        ctx.beginPath();
+        ctx.moveTo(prevCrumb.x, prevCrumb.y);
+        ctx.lineTo(thisCrumb.x, thisCrumb.y);
+        ctx.stroke();
+        prevCrumb = thisCrumb
+    }
+
+    if (this.searchingFood === false) {
+        ctx.beginPath();
+        ctx.moveTo(prevCrumb.x, prevCrumb.y);
+        ctx.lineTo(this.pos.x, this.pos.y);
+        ctx.stroke();
+    }
+
+    if (this.returnedFood) {
+        console.log('Found path')
+        this.game.ctx.font = "bold 24px verdana, sans-serif ";
+        this.game.ctx.fillStyle = "#ff00ff";
+        this.game.ctx.fillText("Food returned", 30, 30);
+    }
 }
 
 function Food(game) {
     this.game = game
     this.pos = {
-        x: this.game.ctx.canvas.width - 30,
-        y: this.game.ctx.canvas.height - 30
+        x: this.game.ctx.canvas.width - 50,
+        y: this.game.ctx.canvas.height - 50
     }
 }
 
@@ -97,7 +131,7 @@ Food.prototype.update = function () {
 
 Food.prototype.draw = function () {
     this.game.ctx.fillStyle = "#FF0000";
-    this.game.ctx.fillRect(this.pos.x, this.pos.y, 16, 16)
+    this.game.ctx.fillRect(this.pos.x, this.pos.y, 20, 20)
 }
 
 function Home(game) {
@@ -116,7 +150,7 @@ Home.prototype.update = function () {
 
 Home.prototype.draw = function () {
     this.game.ctx.fillStyle = "#32CD32";
-    this.game.ctx.fillRect(this.pos.x, this.pos.y, 16, 16)
+    this.game.ctx.fillRect(this.pos.x, this.pos.y, 20, 20)
 }
 
 window.onload = function () {
