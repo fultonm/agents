6e6157684c145bb6d0ae0d86b78c8c3c2ef68759
Michael Fulton <mike@michaelfulton.co>
2019-03-15 17:01:51 -0700

    save/load complete

--- main.js
+++ main.js
@@ -205,7 +205,9 @@ window.onload = function () {
     }
     window.gameEngine = gameEngine
     console.log("All Done!");
-    gameEngine.sock = io.connect('24.16.255.56:8888')
+    gameEngine.sock = io.connect('24.16.255.56:8888', {
+        secure: false
+    })
     gameEngine.sock.on('connect', () => {
         gameEngine.start();
     })
